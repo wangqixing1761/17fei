a0f93c94cc1f1b035d6d12c3ede569960d63c194
Wqx <1761391865@qq.com>
2024-03-04 01:13:20 +0800

    update

--- routes/about.tsx
+++ routes/about.tsx
@@ -14,15 +14,15 @@ export default function About() {
                     <div>
                         <span class="line-through">99</span>49元开通终身会员
                     </div>
-                    <div>支付后添加微信[ wbot10 ] 1~12小时内响应</div>
+                    {/* <div>支付后添加微信[ wbot10 ] 1~12小时内响应</div>
                     <div class="mt-4">
                         如需其他定制开发， 欢迎联系合作。
                         <a href="/custom" class="underline">如何制作一个相同的网站赚钱</a>
-                    </div>
+                    </div> */}
                 </div>
-                <div>
+                {/* <div>
                     <img src="/qrcode.JPG" class="max-w-xs rounded-lg my-4" />
-                </div>
+                </div> */}
             </div>
         </div>
     );
--- routes/unopen.tsx
+++ routes/unopen.tsx
@@ -12,15 +12,13 @@ export default function Page() {
         <img src="/logo.png" class="w-16 h-16" />
         <h2 class="my-4">最新自由定制版即将发布</h2>
         <div class="p-4 border-4 rounded bg-pink-500 border-double">
-          <div>https://17fei.fun</div>
+          {/* <div>https://17fei.fun</div> */}
           <div>支持不同设备访问，不获取隐私信息</div>
         </div>
-        <div class="text-left leading-8 font-black mt-8">
+        {/* <div class="text-left leading-8 font-black mt-8">
           该功能更新维护中(10-26 ~ 10-27)，请使用其他功能
-        </div>
+        </div> */}
         <a class="bg-pink-600 m-8 p-2 border rounded" href="/">返回首页</a>
-        <div class="my-8">关注小红书，获取最新发布动态。</div>
-        <div><a class="underline" href="https://www.xiaohongshu.com/user/profile/64e8ce860000000001013f7a">小红书官方账号</a></div>
       </div>
       <Footer></Footer>
     </div>
