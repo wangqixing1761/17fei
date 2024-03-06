63607d73fab3a0959fbb11d0c9b2155302e502a6
Wqx <1761391865@qq.com>
2024-03-06 20:32:39 +0800

    update

--- routes/about.tsx
+++ routes/about.tsx
@@ -9,16 +9,16 @@ export default function About() {
                 </div>
                 <div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
                     <div class="text-lg text-gray-500">关于本站</div>
-                    <div class="my-4">网站目前只接收客服人工收款</div>
                     <div>情侣互动，增进感情的情趣小游戏（不是情侣也可以玩哦）</div>
-                    {/* <div>
-                        <span class="line-through">99</span>49元开通终身会员
-                    </div> */}
-                    {/* <div>支付后添加微信[ wbot10 ] 1~12小时内响应</div>
+                    <div class="my-4">网站目前只有客服人工收款</div>
+                    <div>
+                        <span class="line-through">10</span>6.9元解锁全部玩法
+                    </div>
+                    <div>添加微信客服[ little-star176 ] 后支付</div>
                     <div class="mt-4">
                         如需其他定制开发， 欢迎联系合作。
-                        <a href="/custom" class="underline">如何制作一个相同的网站赚钱</a>
-                    </div> */}
+                        {/* <a href="/custom" class="underline">如何制作一个相同的网站赚钱</a> */}
+                    </div>
                 </div>
                 <div>
                     <img src="/qrcode.JPG" class="max-w-xs rounded-lg my-4" />
